76d58f813e8ca54b83e345bb2eee48abb0decc7a
work.risotiong <ivan@avit.dev>
2023-06-01 15:58:00 +0800

    feat: add authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/assets/scss/app.scss';
+import { Providers } from './provider';
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
